a87e424d66315f5f303510752de24e3ee14471cb
ManishBobde <manishbobde6@gmail.com>
2020-10-10 12:58:52 +0530

    Updates

---
+++ precache-manifest.a6d2975ccc96d259a49a1cbef46fe1f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcad84e7af9e1178cdd82274936e69cb",
+    "url": "/kanban-demo/index.html"
+  },
+  {
+    "revision": "72938132a65886702074",
+    "url": "/kanban-demo/static/css/main.b11a0ae2.chunk.css"
+  },
+  {
+    "revision": "a15246286390a05cca3f",
+    "url": "/kanban-demo/static/js/2.65309109.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kanban-demo/static/js/2.65309109.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72938132a65886702074",
+    "url": "/kanban-demo/static/js/main.0fd464c0.chunk.js"
+  },
+  {
+    "revision": "0484b6fe05f52b451dc0",
+    "url": "/kanban-demo/static/js/runtime-main.13026f90.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kanban-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
